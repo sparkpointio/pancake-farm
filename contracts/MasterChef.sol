cea5a8b06ee8fc6afa0d858b8e73b4c3e2c4eb90
Aldrick Bonaobra <git@aldrickb.com>
2021-02-19 11:48:28 +0800

    Update emergencyWithdraw function

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -145,10 +145,9 @@ contract MasterChef is Ownable {
     }
 
     // Withdraw without caring about rewards. EMERGENCY ONLY.
-    function emergencyWithdraw(uint256 _pid) public {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+    function emergencyWithdraw() public {
+        UserInfo storage user = userInfo[msg.sender];
+        poolInfo.lpToken.safeTransfer(address(msg.sender), user.amount);
         emit EmergencyWithdraw(msg.sender, _pid, user.amount);
         user.amount = 0;
         user.rewardDebt = 0;
