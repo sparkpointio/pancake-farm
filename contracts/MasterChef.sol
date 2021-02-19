500294d1855932d6f987b7103f9de359e9245fda
Aldrick Bonaobra <git@aldrickb.com>
2021-02-19 11:32:06 +0800

    Remove massUpdatePools function

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -107,15 +107,6 @@ contract MasterChef is Ownable {
         return user.amount.mul(accCakePerShare).div(1e12).sub(user.rewardDebt);
     }
 
-    // Update reward variables for all pools. Be careful of gas spending!
-    function massUpdatePools() public {
-        uint256 length = poolInfo.length;
-        for (uint256 pid = 0; pid < length; ++pid) {
-            updatePool(pid);
-        }
-    }
-
-
     // Update reward variables of the given pool to be up-to-date.
     function updatePool(uint256 _pid) public {
         PoolInfo storage pool = poolInfo[_pid];
